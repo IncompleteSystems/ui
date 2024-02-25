104dc82a90e24a3d9e461ec7e778d7288b1d108d
Matthew Ratzke <matt@degree9.io>
2024-02-25 14:47:57 -0700

    wip

--- packages/appbar/src/types.tsx
+++ packages/appbar/src/types.tsx
@@ -1,10 +1,10 @@
 import type { ComponentPropsWithoutRef } from 'react';
 
-import { UIComponentVariantProps } from '@incomplete/ui';
+import { UIComponentVariantProps, UIComponent } from '@incomplete/ui';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 import { AppBarStyles } from './styles';
 
-export type AppBarProps = ComponentPropsWithoutRef<'aside'> & UIComponentVariantProps<typeof AppBarStyles>;
+export type AppBarProps = UIComponent<'aside', any> & ComponentPropsWithoutRef<'aside'> & UIComponentVariantProps<typeof AppBarStyles>;
 export type AppBarNavProps = ComponentPropsWithoutRef<'nav'> & UIComponentVariantProps<typeof AppBarStyles>;
-export type AppBarNavItemProps = ComponentPropsWithoutRef<'a'> & UIComponentVariantProps<typeof AppBarStyles> & { icon: IconDefinition, as: React.ReactNode };
+export type AppBarNavItemProps = ComponentPropsWithoutRef<'a'> & UIComponentVariantProps<typeof AppBarStyles> & { icon: IconDefinition, as: React.ReactNode | React.ForwardRefExoticComponent<any> };
